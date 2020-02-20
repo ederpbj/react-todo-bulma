3d5edde925d23d91a646b55c51db8036f8d3b3b5
ederpbj <ederpbj@yahoo.com.br>
2020-02-20 19:04:12 -0300

    Conectando ao mongo local

--- src/server/server.js
+++ src/server/server.js
@@ -3,12 +3,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+//Conectando ao mongo
+const mongoose = require('mongoose');
+const config = require('./config/keys.config');
+
 //Identificar e utilizar as rotas
 const TaskRoutes = require('./routes/Task');
-//Definindo rota api/tasks, para exibir todas as tasks
-app.use('/api/tasks', TaskRoutes);
+
+const cors = require('cors');
+
+//Conectar ao mongo
+mongoose.connect(config.mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.log('Error on database connection', err));
+mongoose.set('useFindAndModify', false);
 
 app.use(bodyParser.json());
+app.use(cors());
+
+//Definindo rota api/tasks, para exibir todas as tasks
+app.use('/api/tasks', TaskRoutes);
 
 const port = process.env.PORT || 3001;
 
